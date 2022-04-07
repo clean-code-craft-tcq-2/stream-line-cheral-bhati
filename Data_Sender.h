567edcaffa61d7cc3d5fd1462947132881255720
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-04-07 12:38:15 +0530

    Update Data_Sender.h

--- Data_Sender.h
+++ Data_Sender.h
@@ -2,5 +2,5 @@
 
 #define TotalNoOfReadings 50
 
-bool readDataFromFile(float *Temperature, int *SOC, char *inputFile)
-void SendDataToConsole(float *Temperature, int *SOC)
+bool readDataFromFile(float *Temperature, int *SOC, char *inputFile);
+void SendDataToConsole(float *Temperature, int *SOC);
